4dd6365b6182bd518b06978b8d3075c2ab2d003c
Ed0304 <etaidy001@mymail.sim.edu.sg>
2024-10-15 19:30:06 +0800

    initial commit

---
+++ CSIT314.js
@@ -0,0 +1,3 @@
+function hello_world(){
+    alert("This website is made by:\n Edbert Taidy\n Ehren Thor\n Ceana Joy\n Clare Ng\n Miguel Velasquez\n Mohammed Irfaan\n Harshni Balasubramanian\n");
+}
