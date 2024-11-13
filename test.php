6bfec943d0abe8107d93a5169ef65c32c3aecfbc
mdnfaan <65392809+mdnfaan@users.noreply.github.com>
2024-11-14 04:14:01 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
-$host = 'localhost'; // or the IP address
+$host = '3.25.175.57:3306'; // or the IP address
 $dbname = 'csit314';
 $username = 'root';
 $password = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=localhost;dbname=csit314;unix_socket=/var/lib/mysql/mysql.sock", $username, $password);
     echo "Database connection successful!";
 } catch (PDOException $e) {
     echo "Database connection failed: " . $e->getMessage();
